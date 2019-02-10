7082216a3d216bf5e73f5cfaaf305b42127779e6
ftballpack <39963341+ftballpack@users.noreply.github.com>
2019-02-10 10:42:35 -0600

    Added logic to reduce unnecessary queries.
    Added logic where if the first query comes back with no results, meaning no one is over the login in limit, the script skips to the end, as opposed to unnecessarily running the second SQL query.

--- loginlimit.py
+++ loginlimit.py
@@ -27,6 +27,7 @@ while True:
                                port=port)
 
 
+
      #Query to find token of users over the set limit of logins
      #Subquery finds all userids that are over the limit of allowed logins. The main query than selects all of the tokens from the userids over the login limit set in the config.
      sql_select_Query = "SELECT `token` FROM web_session WHERE `userid` IN (SELECT `userid` FROM (SELECT `userid` FROM web_session WHERE `userid` != '' GROUP BY `userid` HAVING COUNT(*) > %s)t);"
@@ -34,44 +35,50 @@ while True:
      cursor.execute(sql_select_Query, (number_of_concurrent_allowed_user_sessions_per_account , ))
      alltokens = cursor.fetchall()
 
-     #Query to find the tokens of the latest updated user sessions for all users
-     #Subquery sorts all userid sessions by the most recently updated, but retains only those sessions up to the limit of user sessions set in the config, for all userids in the database.
-     #The main query returns back the tokens for all users sessions, but limited to the maximum number set in the config and sorted by most recently updated 
-     sql_select_Query2 = "SELECT `token` FROM web_session s WHERE ( SELECT COUNT(*) FROM web_session f WHERE f.userid = s.userid AND f.updated >= s.`updated` ) <= %s AND `userid` != '' ORDER BY `s`.`updated` DESC;"
-     cursor = conn.cursor()
-     cursor.execute(sql_select_Query2, (number_of_concurrent_allowed_user_sessions_per_account , ))
-     recenttokens = cursor.fetchall()
-
-     #for printing out text for testing query results
-     #print (alltokens)
-     #print ("-----Line Break-----")
-     #print (recenttokens)
-     #print ("-----Line Break-----")
-
-     #Retaining newest user session(s) only
-     #This iteration takes the results of the first query (all tokens of those exceeding the login limit) and iterates through and removes the results of the second query, (the tokens from the most recently updated users sessions for each user, up the limit set in the config for each user)
-     #The end results of the iteration is only the tokens of users, exceeding the login limit, but removing the tokens from the most recent user sessions, set in the config, are retained in the tokenstobedeleted variable.
-     tokenstobedeleted = [x for x in alltokens if x not in recenttokens]
-
-     #for testing, printing out results to be deleted
-     #print (tokenstobedeleted)
-
-     #If the variable tokenstobedeleted is empty, the script posts no users are in violation of the user session limits and proceeds to the end of the script, avoiding an additional, unnecessary, DB query.
-     if tokenstobedeleted == []:
+     if alltokens == []:
          print(" ")
          print("No Users in Violation of Login Limit, No User Tokens or Sessions Removed from Database")
          cursor.close()
 
-     #Else if the variable tokenstobedeleted is not empty, the delete query is executed and the tokens contained in the tokenstobedeleted variable are deleted from the mysql database.
      else:
-         sql_Delete_query = """DELETE FROM web_session WHERE `token` = %s;"""
+         #Query to find the tokens of the latest updated user sessions for all users
+         #Subquery sorts all userid sessions by the most recently updated, but retains only those sessions up to the limit of user sessions set in the config, for all userids in the database.
+         #The main query returns back the tokens for all users sessions, but limited to the maximum number set in the config and sorted by most recently updated
+         sql_select_Query2 = "SELECT `token` FROM web_session s WHERE ( SELECT COUNT(*) FROM web_session f WHERE f.userid = s.userid AND f.updated >= s.`updated` ) <= %s AND `userid` != '' ORDER BY `s`.`updated` DESC;"
          cursor = conn.cursor()
-         cursor.executemany(sql_Delete_query, tokenstobedeleted)
-         conn.commit()
-         print(" ")
-         print(cursor.rowcount, "Users Session(s) Deleted Successfully.")
-         print("Users Found in Violation of Login Limit, Session(s) Deleted successfully, Token(s) Deleted from Database ")
-         cursor.close()
+         cursor.execute(sql_select_Query2, (number_of_concurrent_allowed_user_sessions_per_account , ))
+         recenttokens = cursor.fetchall()
+
+         #for printing out text for testing query results
+         #print (alltokens)
+         #print ("-----Line Break-----")
+         #print (recenttokens)
+         #print ("-----Line Break-----")
+
+         #Retaining newest user session(s) only
+         #This iteration takes the results of the first query (all tokens of those exceeding the login limit) and iterates through and removes the results of the second query, (the tokens from the most recently updated users sessions for each user, up the limit set in the config for each user)
+         #The end results of the iteration is only the tokens of users, exceeding the login limit, but removing the tokens from the most recent user sessions, set in the config, are retained in the tokenstobedeleted variable.
+         tokenstobedeleted = [x for x in alltokens if x not in recenttokens]
+
+         #for testing, printing out results to be deleted
+         #print (tokenstobedeleted)
+
+         #If the variable tokenstobedeleted is empty, the script posts no users are in violation of the user session limits and proceeds to the end of the script, avoiding an additional, unnecessary, DB query.
+         if tokenstobedeleted == []:
+             print(" ")
+             print("No Users in Violation of Login Limit, No User Tokens or Sessions Removed from Database")
+             cursor.close()
+
+         #Else if the variable tokenstobedeleted is not empty, the delete query is executed and the tokens contained in the tokenstobedeleted variable are deleted from the mysql database.
+         else:
+             sql_Delete_query = """DELETE FROM web_session WHERE `token` = %s;"""
+             cursor = conn.cursor()
+             cursor.executemany(sql_Delete_query, tokenstobedeleted)
+             conn.commit()
+             print(" ")
+             print(cursor.rowcount, "Users Session(s) Deleted Successfully.")
+             print("User(s) Found in Violation of Login Limit, Session(s) Deleted successfully, Token(s) Deleted from Database ")
+             cursor.close()
 
 
   except Error as e :
